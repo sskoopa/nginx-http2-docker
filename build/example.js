d949a3c7024531c7ce7488cab5b64a6254d2a395
sskoopa <scott@koopaworks.com>
2018-11-28 03:35:47 +0000

    added njs to nginx config

---
+++ build/example.js
@@ -0,0 +1,29 @@
+function version(r) {
+    r.return(200, njs.version);
+}
+
+function hello(r) {
+    r.return(200, "Hello world!\n");
+}
+
+function join(r) {
+    join_subrequests(r, ['/foo', '/bar']);
+}
+
+function join_subrequests(r, subs) {
+    var parts = [];
+
+    function done(reply) {
+        parts.push({ uri:  reply.uri,
+                     code: reply.status,
+                     body: reply.responseBody });
+
+        if (parts.length == subs.length) {
+            r.return(200, JSON.stringify(parts));
+        }
+    }
+
+    for (var i in subs) {
+        r.subrequest(subs[i], done);
+    }
+}
