66b3e645b0188f660975d90df41a7a6aada0b998
sskoopa <scott@koopaworks.com>
2018-11-27 19:37:05 -0800

    add css for html

--- build/example.js
+++ build/example.js
@@ -1,29 +1,53 @@
 function version(r) {
-    r.return(200, njs.version);
+  
+  r.return(200, njs.version);
 }
 
 function hello(r) {
-    r.return(200, "Hello world!\n");
+  r.return(200, "Hello world!\n");
+}
+
+function dec_foo(r) {
+  return decodeURIComponent(r.args.foo);
+}
+
+function inject_header(s) {
+  inject_my_header(s, 'Content-Type: text/plain');
+}
+
+function inject_my_header(s, header) {
+  var req = '';
+
+  s.on('upload', function(data, flags) {
+      req += data;
+      var n = req.search('\n');
+      if (n != -1) {
+          var rest = req.substr(n + 1);
+          req = req.substr(0, n + 1);
+          s.send(req + header + '\r\n' + rest, flags);
+          s.off('upload');
+      }
+  });
 }
 
 function join(r) {
-    join_subrequests(r, ['/foo', '/bar']);
+  join_subrequests(r, ['/hello', '/version']);
 }
 
 function join_subrequests(r, subs) {
-    var parts = [];
+  var parts = [];
 
-    function done(reply) {
-        parts.push({ uri:  reply.uri,
-                     code: reply.status,
-                     body: reply.responseBody });
+  function done(reply) {
+      parts.push({ uri:  reply.uri,
+                   code: reply.status,
+                   body: reply.responseBody });
 
-        if (parts.length == subs.length) {
-            r.return(200, JSON.stringify(parts));
-        }
-    }
+      if (parts.length == subs.length) {
+          r.return(200, JSON.stringify(parts));
+      }
+  }
 
-    for (var i in subs) {
-        r.subrequest(subs[i], done);
-    }
+  for (var i in subs) {
+      r.subrequest(subs[i], done);
+  }
 }
